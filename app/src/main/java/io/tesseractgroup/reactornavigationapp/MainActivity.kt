ba024cacf6cdc5be797182f71e2ce3349a0f153a
Matthew McArthur <matthew.s.mcarthur@gmail.com>
2019-08-21 15:02:37 -0600

    update dependencies

--- app/src/main/java/io/tesseractgroup/reactornavigationapp/MainActivity.kt
+++ app/src/main/java/io/tesseractgroup/reactornavigationapp/MainActivity.kt
@@ -1,18 +1,17 @@
 package io.tesseractgroup.reactornavigationapp
 
-import android.os.Bundle
 import android.util.Log
+import io.tesseractgroup.purestatemachine.StateUpdate
 import io.tesseractgroup.reactor.Core
-import io.tesseractgroup.reactor.CoreUpdate
 import io.tesseractgroup.reactornavigation.*
 
 object App {
-    private val handler = fun(state: AppState, event: AppEvent): CoreUpdate<AppState, AppCommand> {
+    private val handler = fun(state: AppState, event: AppEvent): StateUpdate<AppState, AppCommand> {
         Log.d("NAVIGATION", "Stack: $event}")
         when (event) {
             is AppEvent.UpdateName -> {
                 val updatedState = state.copy(name = event.name)
-                return CoreUpdate.State(updatedState)
+                return StateUpdate.State(updatedState)
             }
         }
     }
